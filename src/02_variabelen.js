9a8a055eab962eeec67eba98988313752c1b87c2
SCOTCH / Hubert Baier <hubert.baier@freenet.de>
2024-07-01 10:03:38 +0000

    Ändereung

--- src/02_variabelen.js
+++ src/02_variabelen.js
@@ -10,7 +10,10 @@
 
 /***** Variablen 01 *****/
 
-console.log("hi");
-console.log(hi);
+// console.log("hi");
+// console.log(Hi);
 
+/* Deklaration + Wertzuweisung I */
 
+let firstName; // Deklaration (Defininition)
+console.log(let firstName)); // Ausagabe
