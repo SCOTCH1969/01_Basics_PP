282b19b6c495a1b04647481f15f8d39db139087a
SCOTCH / Hubert Baier <hubert.baier@freenet.de>
2024-07-04 09:06:42 +0000

    8 Mehrere Parameter

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -27,5 +27,16 @@ function ausgabeNamen() {
     console.log("Hallo, " + firstName +  " ! ");
     
 }
+// console.log(firstName);  // Fehler : scope !
 
-console.log(firstName);
+/***** Funktionen 02b ******/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN 
+
+ausgabeNamen2("Arian"); // Argument --> Daten für Parameter
+ausgabeNamen2("Maxim");
+ausgabeNamen2("Saba");
+
+function ausgabeNamen2(firstName2) { //Parameter
+    console.log("Hallo, " + firstName2 + " !" );   
+}
+
