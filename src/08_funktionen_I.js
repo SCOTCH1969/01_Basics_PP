34063aaf01bb97281f96f4c87661c60574413fb1
SCOTCH / Hubert Baier <hubert.baier@freenet.de>
2024-07-04 08:27:39 +0000

    Ausgabe Namen geändert

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,3 +2,30 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+
+
+
+
+
+
+
+
+
+
+
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Call
+ausgabeNamen();
+
+function ausgabeNamen() {
+
+console.log("Hallo, Marius ! ");
+console.log(" ");
+console.log("Hallo, Sara ! ");
+console.log(" ");
+console.log("Hallo, Saba ! ");
+console.log(" ");
+}
