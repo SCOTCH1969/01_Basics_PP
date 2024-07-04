ae1ae904bae7a2f954844a6fef9551cd10541a38
SCOTCH / Hubert Baier <hubert.baier@freenet.de>
2024-07-04 06:52:23 +0000

    Codechallenge 2 geladen

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -13,8 +13,23 @@ isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
+console.log(" ")
 console.log("ageMark: " + ageMark);
+console.log(" ")
 console.log("isJohnOlder: " + isJohnOlder);
+console.log(" ")
 console.log("isJohnEqual: " + isJohnEqual);
+console.log(" ")
 console.log("------------------");
+console.log(" ")
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+if(true){
+    console.log("John ist älter. ")
+    console.log(" ")
+}
 
