943c51e363c5befac7b342a39564f9400645cace
SCOTCH / Hubert Baier <hubert.baier@freenet.de>
2024-07-08 06:33:17 +0000

    9 Änderung

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,11 +10,31 @@
 4. Ausgabe in Konsole : 
 */
 
+// module: addition a + b |  test:
+// output(add(2,2));
+// output(add(2-2));
+// output(add(2,0));
+// function add(a,b) {
+// 	return a + b;
+// }
 
 
+// module: subtraction a - b |  test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,2));
+// function subtract(a,b) {
+// 	return a - b;}
 
-
-
+// module: division a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+output(divide(0,0));
+function divide(a,b);
 
 
 // module: output | test:
